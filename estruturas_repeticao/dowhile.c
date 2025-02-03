c1e3632f88104dd801c36821ab127c1960d2b7ac
Matheus Pessoa <matheus41.dev@gmail.com>
2025-02-03 05:42:56 -0300

    estruturas de repetição ok

---
+++ estruturas_repeticao/dowhile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1; // Variavel de incremento
+
+    do {
+        printf("%d\n", i); // Saída
+        i++; // Incremento
+    } while (i == 5); // Condição
+
+    return 0;
+}
---
+++ estruturas_repeticao/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1; // Variável de incremento
+
+    while (i <= 5) { // Condição
+        printf("%d\n", i); // Saída
+        i++; // Incremento
+    }
+    
+    return 0;
+}
