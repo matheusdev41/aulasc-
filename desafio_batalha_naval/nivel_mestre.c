7bed47caa65bc1516a80dbaf1516f2eedacd84b2
Matheus Pessoa <matheus41.dev@gmail.com>
2025-02-19 21:19:18 -0300

    ok

--- desafio_batalha_naval/nivel_mestre.c
+++ desafio_batalha_naval/nivel_mestre.c
@@ -7,6 +7,8 @@ int main()
 {
     int tabuleiro[LINHAS][COLUNAS];
     int cone[2] = {0, 2};
+    int cruz[2] = {6, 3};
+
     // Inicializa a matriz 10x10
     for (int i = 0; i < LINHAS; i++){
         for (int j = 0; j < COLUNAS; j++){
@@ -16,31 +18,49 @@ int main()
 
     // Cone
     for (int j = 0; j < 3; j++){
-        if (j == 0){
-            int x = cone[0]; 
-            int y = cone[1];
-            if (x < LINHAS && y < COLUNAS && tabuleiro[x][y] != 3){
-                tabuleiro[x][y] = 3;
-            }   
-
-        } else if (j == 1){
-            int x = cone[0] + j;
-            int y = cone[1] - j;
-            if (x < LINHAS && y < COLUNAS && tabuleiro[x][y] != 3){
-                for (int n = 0; n < 3; n++){
-                    tabuleiro[x][y + n] = 3;
-                } 
-            }
-
-        } else if (j == 2) {
-            int x = cone[0] + j;
-            int y = cone[1] - j;
-            if (x < LINHAS && y < COLUNAS && tabuleiro[x][y] != 3){
-                for (int n = 0; n < 5; n++){
-                    tabuleiro[x][y + n] = 3;
-                }
-            }
+        int x = cone[0] + j; 
+        int y = cone[1] - j;
+    
+        if (x >= LINHAS || y < 0){
+            continue; // Condição para evitar acesso fora da matriz
         }
+
+        int largura = 1 + (2 * j); // Largura de cada linha do cone
+        int inicio = y;
+        int fim = y + largura - 1;
+
+        // Evita acesso fora do tabuleiro
+        if (inicio < 0){
+            inicio = 0;
+        }
+        if (fim >= COLUNAS) {
+            fim = COLUNAS - 1;
+        }
+
+        for (int n = inicio; n <= fim; n++){
+            tabuleiro[x][n] = 1;
+        }
+    
+    }
+
+    // Cruz
+    for (int j = 0; j < 5; j++){
+        // Posiciona coluna da cruz
+        int x = cruz[0];
+        int y = cruz[1] + j;
+
+        // Condição para evitar acesso fora da matriz 
+        if(x < LINHAS || y < COLUNAS){
+            continue;
+        }
+
+        tabuleiro[x][y] = 1;
+        for ( int n = 0; n < 3; n++){
+            int x = (cruz[0] - 1) + n;
+            int y = cruz[1] + 2;
+            tabuleiro[x][y] = 1;
+        } 
+
     }
     
     // Printa o tabuleiro
